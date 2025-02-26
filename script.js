458dbdb9e22b9ed2a11be12844ba605ba61294db
Marlon Grochoska <nx250tornado2@gmail.com>
2025-02-26 23:13:06 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("search-button").addEventListener("click", () => {
+        alert("Busca ainda não implementada.");
+    });
+
+    document.getElementById("show-atms").addEventListener("click", () => {
+        alert("Exibir apenas ATMs (ainda não implementado).");
+    });
+
+    document.getElementById("show-stores").addEventListener("click", () => {
+        alert("Exibir apenas Lojas (ainda não implementado).");
+    });
+
+    document.getElementById("show-favorites").addEventListener("click", () => {
+        alert("Exibir Favoritos (ainda não implementado).");
+    });
+});
